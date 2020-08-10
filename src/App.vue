0652b7a03a37633ab311589f0803eb6491ef6d75
SoleneLivran <s.livran@gmail.com>
2020-08-11 00:42:57 +0200

    design v1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/super-mario.png" width="200px">
+    <!-- <img alt="Vue logo" src="./assets/super-mario.png" width="150px"> -->
     <Game></Game>
   </div>
 </template>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -150,22 +150,22 @@ export default {
 
 <template>
 
-	<div>
-		<div id="Game" v-if="!hasWon && !hasLost">
-			<h1>Let's-a-play!</h1>
-			<p class="description">
-				You are the ( <span class="cellCurrent"></span> ), move with your keyboard's arrow keys. &#8592;&#8594;&#8593;&#8595;<br>
-				Collect as many coins ( <span class="coinCell"></span> ) as possible and reach the blue cell!<br>
-				If you find a mushroom ( <span class="mushroomCell"></span> ), you get an extra life &#9829;<br>
-				If you find a star ( <span class="starCell"></span> ), you don't fear the next ennemy &#128170;<br>
-				Ennemies are hidden. If one finds you, you lose a life :(<br>
-				If your lives go down to zero, you lost &#9760;
-			</p>
-			<p>
-				Lives : <span v-for="i in lives" :key="i">&#9829;</span><br>
-				Coins collected: {{playerCoins}}<br>
-				Hidden ennemies: {{ennemies}}
-			</p>
+	<div class="container">
+		<div id="game" v-if="!hasWon && !hasLost">
+			<div class="rules">
+				<h1>Let's-a-play!</h1>
+				<span class="cellCurrent"></span> Move with your keyboard's arrow keys. &#8592;&#8594;&#8593;&#8595;<br>
+				<span class="coinCell"></span> Collect as many coins as possible and reach the flag!<br>
+				<span class="mushroomCell"></span> If you find a mushroom, you get an extra life <i class="fas fa-heart" style="color:red;"></i><br>
+				<span class="starCell"></span> If you find a star, you are protected from the next ennemy<br>
+				<i class="fas fa-skull" style="color:red; font-size:1.5em;"></i> Ennemies are hidden. If one finds you, you lose a life
+				<p>If your lives go down to zero, game over!</p>
+			</div>
+			<div class="stats">
+				<p>Lives : <span v-for="i in lives" :key="i"> <i class="fas fa-heart" style="color:red;"></i> </span></p>
+				<p>Coins collected: {{playerCoins}}</p>
+				<p>Hidden ennemies: {{ennemies}}</p>
+			</div>
 			<div id="board">
 				<!-- TODO start loop at 0 so we don't need the -1s -->
 				<div v-for="row in rows" class="cellRow" :key="row">
@@ -188,9 +188,10 @@ export default {
 		</div>
 
 		<div class="victory" v-if="hasWon">
+			
+			<h1>Victory!</h1>
 			<p>
-				Victory!<br>
-				Coins collected: {{playerCoins}} / {{gameCoins}}
+				Coins collected : {{playerCoins}} / {{gameCoins}}
 			</p>
 			<button class="newGameButton" @click="newGame">
 				New Game
@@ -207,9 +208,6 @@ export default {
 			</button>
 		</div>
 
-		<div class="footer">
-			<a href="https://github.com/SoleneLivran/mario-game"><i class="fab fa-github"></i></a>
-		</div>
 	</div>
 
 </template>
@@ -217,104 +215,158 @@ export default {
 <style>
 	body {
 	padding: 1rem;
-	font-family: Helvetica, sans-serif;
+	background-color:#6096FF;
+	position: relative;
 	}
 
-	.footer {
+	/* .background_images--cloud1 {
+		width: 20%;
+		top: -10%;
+		left: 2%;
+	} */
+
+	h1 {
+		font-family: 'Press Start 2P', cursive;
+	}
+
+	.container {
+		font-family: 'Orbitron', sans-serif;
+		font-weight: 800;
+		color: white;
+	}
+
+	.rules {
+		display: inline-block;
+		padding: 2rem;
+		line-height: 2em;
+		text-align: justify;
+		background-color: #CB4F0E;
+		border-radius: 5px;
+		border-top: 2px solid #FFBFB3;
+		border-left: 2px solid #FFBFB3;
+		border-bottom: 3px solid #120F16;
+		border-right: 3px solid #120F16;
+	}
+
+	.rules p, h1 {
+		text-align: center;
+	}
+
+	.rules h1 {
+		text-shadow: 3px 6px 0px rgba(0, 0, 0, 1);
+	}
+
+	.stats {
+		display: flex;
+		justify-content: center;
+	}
+
+	.stats p {
+		padding: 2em 1em 0 1em;
+	}
+
+	/* .footer {
 		margin-top: 10rem;
 		font-size: 1.5em;
-	}
+	} */
 	
-	a {
+	/* a {
 		color: black;
-	}
+	} */
 
 	.label {
-	font-weight: bold;
-	margin: 1rem 0;
+		font-weight: bold;
+		margin: 1rem 0;
 	}
 
 	#userCode {
-	padding: 1rem;
-	font-family: monospace;
-	background-color: black;
-	color: greenyellow;
-	font-size: 1.4rem;
-	width: 100%;
-	height: 10rem;
+		padding: 1rem;
+		font-family: monospace;
+		background-color: black;
+		color: greenyellow;
+		font-size: 1.4rem;
+		width: 100%;
+		height: 10rem;
 	}
 
 	#launchScript {
-	background-color: greenyellow;
-	border: 0;
-	margin: 0.625rem 0;
-	padding: 0.625rem;
-	border-radius: 0.3rem;
-	cursor: pointer;
+		background-color: greenyellow;
+		border: 0;
+		margin: 0.625rem 0;
+		padding: 0.625rem;
+		border-radius: 0.3rem;
+		cursor: pointer;
 	}
 
 	#board {
-	margin-top: 1rem;
-	display: inline-block;
-	background-color: black;
-	border: 1px solid black;
+		margin-top: 1rem;
+		display: inline-block;
+		background-color: #FEC0B3;
+		border: 1px solid #FEC0B3;
 	}
 
 	.cellRow {
-	display: flex;
+		display: flex;
 	}
 
 	.cell {
-	background-color: lightgrey;
-	width: 3rem;
-	height: 3rem;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	font-weight: bold;
-	font-size: 2em;
-	margin: 1px;
+		background-color: #00AB00;
+		background-size: 100%;
+		opacity: 0.9;
+		width: 5rem;
+		height: 5rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-weight: bold;
+		font-size: 2em;
+		margin: 1px;
 	}
 
 	.cellStart {
-	background-color: green;
+		background-color: green;
 	}
 
 	.cellEnd {
-	background-color: blue;
+		background-image: url("../../public/img/finish.png");
+		background-size: 80%;
+		background-repeat: no-repeat;
+		background-position: center;
 	}
 
-	.cellCurrent::after {
-	content: "X";
-	color: red;
-	}
-
-	.victory {
-	color: red;
-	margin: 1em;
-	font-weight: bold;
-	font-size: 2em;
+	.cellCurrent {
+		background-image: url("../../public/img/mario-jump.png");
+		background-size: 80%;
+		background-repeat: no-repeat;
+		background-position: center;
+		/* content: url("../../public/img/mario-jump.png"); */
 	}
 
 	.coinCell::after {
-	content: "0";
-	color: rgba(255, 196, 0, 0.904);
+		content: url("../../public/img/coin.gif");
 	}
 
 	.mushroomCell::after {
-	content: "m";
-	color: rgba(109, 53, 15, 0.692);
+		content: url("../../public/img/mushroom.gif");
 	}
 
 	.starCell::after {
-	content: "*";
-	color: rgba(41, 187, 255, 0.938);
+		content: url("../../public/img/star.gif");	
 	}
 
 	/* .ennemyCell::after {
 	content: ":(";
 	color: rgba(48, 51, 53, 0.938);
 	} */
+
+	.victory {
+		margin: 1em;
+		font-size: 2em;
+	}
+
+	.victory h1 {
+		font-family: 'Press Start 2P', cursive;
+	}
 
 
 </style>
